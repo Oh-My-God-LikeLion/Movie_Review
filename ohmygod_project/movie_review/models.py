83a91ac4a2a8a8468f4d406b0f341be51a83f9b4
Haejin <hj0816hj@gmail.com>
2021-05-01 14:43:50 +0900

    edit models.py

--- ohmygod_project/movie_review/models.py
+++ ohmygod_project/movie_review/models.py
@@ -1,3 +1,38 @@
+from __future__ import unicode_literals  # 문자 인코딩
+from django.utils import timezone
 from django.db import models
 
+
 # Create your models here.
+
+class Content(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=timezone.now)  # 등록 시점
+    review = models.TextField(default='')
+
+    plot = models.TextField(default='')
+    release_date = models.DateField()
+    poster = models.ImageField()
+
+    action = '액션'
+    thriller = '스릴러'
+    romance = '로맨스'
+    comdedy = '코미디'
+    musical = '뮤지컬'
+    fantasy = '판타지'
+    animation = '애니메이션'
+    GENRE_CHOICES = [
+        (action, '액션'),
+        (thriller, '스릴러'),
+        (romance, '로맨스'),
+        (comdedy, '코미디'),
+        (musical, '뮤지컬'),
+        (fantasy, '판타지'),
+        (animation, '애니메이션'),
+    ]
+    genre = models.CharField(
+        max_length=10,
+        choices=GENRE_CHOICES,
+        default=action
+    )
+    score = models.IntegerField(default=0)
